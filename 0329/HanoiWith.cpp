dc8170a47a3fe6c427ea33246b5b2d8adc2fd3d7
Chuck LU <chucklu2015@outlook.com>
2025-03-29 17:33:13 +0800

    show disk number for each move

--- 0329/HanoiWith.cpp
+++ 0329/HanoiWith.cpp
@@ -1,35 +1,73 @@
 #include <iostream>
+#include <stack>
 using namespace std;
-int diskNumber=0;
-int diskCount = 2;
+stack<int> stackA;
+stack<int> stackB;
+stack<int> stackC;
 int steps = 0;
 
 void move(string source, string destination)
 {
     steps++;
-    cout << "steps" << steps << ": move " << diskNumber << " from " << source << " to " << destination << endl;
+    int diskNumber = 0;
+    if (source == "A")
+    {
+        diskNumber = stackA.top();
+        stackA.pop();
+    }
+
+    if (source == "B")
+    {
+        diskNumber = stackB.top();
+        stackB.pop();
+    }
+
+    if (source == "C")
+    {
+        diskNumber = stackC.top();
+        stackC.pop();
+    }
+
+    if (destination == "A")
+    {
+        stackA.push(diskNumber);
+    }
+
+    if (destination == "B")
+    {
+        stackB.push(diskNumber);
+    }
+
+    if (destination == "C")
+    {
+        stackC.push(diskNumber);
+    }
+
+    cout << "steps" << steps << ": move disk " << diskNumber << " from " << source << " to " << destination << endl;
 }
 
 void hanoi(int n, string source, string middle, string destination)
 {
-    cout<<"n="<<n<<", source="<<source<<", middle="<<middle<<", destination="<<destination<<endl;
+    //cout << "n=" << n << ", source=" << source << ", middle=" << middle << ", destination=" << destination << endl;
     if (n == 1)
     {
         move(source, destination);
     }
     else
     {
-        diskNumber++;
         hanoi(n - 1, source, destination, middle);
-        diskNumber++;
         move(source, destination);
-        diskNumber--;
         hanoi(n - 1, middle, source, destination);
     }
 }
 
 int main()
 {
-    hanoi(diskCount, "A", "B", "C");
+    int totalDiskCount = 3;
+    for (int i = totalDiskCount; i > 0; i--)
+    {
+        stackA.push(i);
+    }
+    hanoi(totalDiskCount, "A", "B", "C");
     return 0;
 }
