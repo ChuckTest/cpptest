f42f8c2b0f76d5316a3edb9aa8bb36629a42a3b0
Chuck LU <chucklu2015@outlook.com>
2025-03-29 09:14:02 +0800

    sample 3-1

---
+++ ch03/03_01.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+double power(double x, int n)
+{
+    double result = 1.0;
+    while (n > 0)
+    {
+        result = result * x;
+        n--;
+    }
+    return result;
+}
+
+int main()
+{
+    double x = 5;
+    int n = 2;
+    double result = power(x, n);
+    cout << "5 to the power 2 is " << result << endl;
+}
