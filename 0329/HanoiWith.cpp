bf3d7ae5ac2ae523382ebac413cf32a1b7b38c85
Chuck LU <chucklu2015@outlook.com>
2025-03-29 10:58:18 +0800

    cout each source, middle and destination

--- 0329/HanoiWith.cpp
+++ 0329/HanoiWith.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
 using namespace std;
-int diskNumber;
+int diskNumber=0;
 int diskCount = 2;
 int steps = 0;
 
-void move(int n, string source, string destination)
+void move(string source, string destination)
 {
     steps++;
-    cout << "steps" << steps << ": move " << n << " from " << source << " to " << destination << endl;
+    cout << "steps" << steps << ": move " << diskNumber << " from " << source << " to " << destination << endl;
 }
 
 void hanoi(int n, string source, string middle, string destination)
 {
+    cout<<"n="<<n<<", source="<<source<<", middle="<<middle<<", destination="<<destination<<endl;
     if (n == 1)
     {
-        move(diskCount, source, destination);
+        move(source, destination);
     }
     else
     {
+        diskNumber++;
         hanoi(n - 1, source, destination, middle);
-        move(n - 1, source, destination);
+        diskNumber++;
+        move(source, destination);
+        diskNumber--;
         hanoi(n - 1, middle, source, destination);
     }
 }
