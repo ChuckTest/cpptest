fe1c827598513c9c90a73078015974e0ff35ff7a
Chuck LU <chucklu2015@outlook.com>
2025-03-29 19:08:43 +0800

    use sqrt outside the for loop

--- 0329/teacher/202 优化for循环穷举.cpp
+++ 0329/teacher/202 优化for循环穷举.cpp
@@ -5,71 +5,108 @@ using namespace std;
 
 clock_t start, endTime; // Renamed 'end' to 'endTime'
 
-int main() {
-    int x, i, n = 0;
-    int num = 0;
+int main()
+{
+	int x, i, n = 0;
+	int num = 0;
 
-    // Regular solution
-    start = clock();
-    for(x = 2; x <= 90000; x++) {
-        n = 0;
-        for(i = 1; i <= x; i++)
-            if(x % i == 0)
-                n++;
-        if(n == 2)
-            num++;
-    }
-    cout << "There are " << num << " prime numbers" << endl;
-    endTime = clock();  // Renamed to 'endTime'
-    cout << "The original program duration is: " << endTime - start << " milliseconds" << endl << endl;
+	// Regular solution
+	start = clock();
+	for (x = 2; x <= 90000; x++)
+	{
+		n = 0;
+		for (i = 1; i <= x; i++)
+			if (x % i == 0)
+				n++;
+		if (n == 2)
+			num++;
+	}
+	cout << "There are " << num << " prime numbers" << endl;
+	endTime = clock(); // Renamed to 'endTime'
+	cout << "The original program duration is: " << endTime - start << " milliseconds" << endl
+		 << endl;
 
-    // Optimization 1
-    num = 0;
-    start = clock();
-    for(x = 2; x <= 90000; x++) {
-        for(i = 2; i < x; i++)
-            if(x % i == 0)
-                break;
-        if(i == x)
-            num++;
-    }
-    cout << "There are " << num << " prime numbers" << endl;
-    endTime = clock();  // Renamed to 'endTime'
-    cout << "The program V1 duration is: " << endTime - start << " milliseconds" << endl << endl;
+	// Optimization 1
+	num = 0;
+	start = clock();
+	for (x = 2; x <= 90000; x++)
+	{
+		for (i = 2; i < x; i++)
+			if (x % i == 0)
+				break;
+		if (i == x)
+			num++;
+	}
+	cout << "There are " << num << " prime numbers" << endl;
+	endTime = clock(); // Renamed to 'endTime'
+	cout << "The program V1 duration is: " << endTime - start << " milliseconds" << endl
+		 << endl;
 
-    // Optimization 2
-    num = 0;
-    start = clock();
-    for(x = 2; x <= 90000; x++) {
-        n = 0;
-        for(i = 2; i <= x / 2; i++)
-            if(x % i == 0) {
-                n++;
-                break;
-            }
-        if(n == 0)
-            num++;
-    }
-    cout << "There are " << num << " prime numbers" << endl;
-    endTime = clock();  // Renamed to 'endTime'
-    cout << "The program V2 duration is: " << endTime - start << " milliseconds" << endl << endl;
+	// Optimization 2
+	num = 0;
+	start = clock();
+	for (x = 2; x <= 90000; x++)
+	{
+		n = 0;
+		for (i = 2; i <= x / 2; i++)
+			if (x % i == 0)
+			{
+				n++;
+				break;
+			}
+		if (n == 0)
+			num++;
+	}
+	cout << "There are " << num << " prime numbers" << endl;
+	endTime = clock(); // Renamed to 'endTime'
+	cout << "The program V2 duration is: " << endTime - start << " milliseconds" << endl
+		 << endl;
 
-    // Optimization 3
-    num = 0;
-    start = clock(); 
-    for(x = 2; x <= 90000; x++) {
-        n = 0;
-        for(i = 2; i <= sqrt(x); i++)
-            if(x % i == 0) {
-                n++;
-                break;
-            }
-        if(n == 0)
-            num++;
-    }
-    cout << "There are " << num << " prime numbers" << endl;
-    endTime = clock();  // Renamed to 'endTime'
-    cout << "The program V3 duration is: " << endTime - start << " milliseconds" << endl << endl;
+	// Optimization 3
+	num = 0;
+	start = clock();
+	for (x = 2; x <= 90000; x++)
+	{
+		for (i = 2; i <= sqrt(x); i++)
+			if (x % i == 0)
+			{
+				n++;
+				break;
+			}
+		if (n == 0)
+			num++;
+	}
+	cout << "There are " << num << " prime numbers" << endl;
+	endTime = clock(); // Renamed to 'endTime'
+	cout << "The program V3 duration is: " << endTime - start << " milliseconds" << endl
+		 << endl;
 
-    return 0;
+	num = 0;
+	start = clock();
+
+	for (x = 2; x <= 90000; x++)
+	{
+		bool isPrime = true; // Assume x is prime unless proven otherwise
+		int sqrtX = sqrt(x); // Precompute sqrt(x) to avoid repeated calls
+
+		for (i = 2; i <= sqrtX; i++)
+		{
+			if (x % i == 0)
+			{ // If x is divisible by i, it's not prime
+				isPrime = false;
+				break;
+			}
+		}
+
+		if (isPrime)
+		{
+			num++; // Increment prime count
+		}
+	}
+
+	cout << "There are " << num << " prime numbers" << endl;
+	endTime = clock(); // Renamed to 'endTime'
+	cout << "The program V4 duration is: " << endTime - start << " milliseconds" << endl
+		 << endl;
+	return 0;
 }
