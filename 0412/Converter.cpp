b8bdc18c74dc650f5bae0dcffb1ebf48fd0beaf2
Chuck LU <chucklu2015@outlook.com>
2025-04-12 11:23:24 +0800

    decimal to binary

--- 0412/Converter.cpp
+++ 0412/Converter.cpp
@@ -1,30 +1,53 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
+#include <stack>
 using namespace std;
-#include<cmath>
 
-class Converter{
-    public:
-    static void DecimalToBinary(int number){
+class Converter
+{
+private:
+    stack<int> tempStack;
 
+public:
+    void DecimalToBinary(int number)
+    {
+        int temp = number;
+        int remainder = 0;
+        while (temp != 0)
+        {
+            remainder = temp % 2;
+            tempStack.push(remainder);
+            temp = temp / 2;
+        }
+        //tempStack.push(0);
+        while (!tempStack.empty())
+        {
+            cout << tempStack.top();
+            tempStack.pop();
+        }
     }
 
-    static void BinaryToDecimal(string str){
-        int length=str.size();
-        int number=0;
-        int count=0;
-        for(int i=length-1;i>=0;i--){
-            if(str[i]=='1'){
-                number+=pow(2,count);
+    void BinaryToDecimal(string str)
+    {
+        int length = str.size();
+        int number = 0;
+        int count = 0;
+        for (int i = length - 1; i >= 0; i--)
+        {
+            if (str[i] == '1')
+            {
+                number += pow(2, count);
             }
             count++;
         }
-        cout<<str<<" map to decimal "<<number<<endl;
+        cout << str << " map to decimal " << number << endl;
     }
-
 };
 
-int main(){
-    Converter::BinaryToDecimal("11010101");
-    Converter::DecimalToBinary(15);
+int main()
+{
+    Converter con;
+    con.BinaryToDecimal("11010101");
+    con.DecimalToBinary(15);
     return 0;
 }
