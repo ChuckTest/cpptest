2cad52287771fd20aa5f8c3f433594ad05101a76
Chuck LU <chucklu2015@outlook.com>
2025-06-13 18:22:09 +0800

    another order test

---
+++ test_order_003.cpp
@@ -0,0 +1,12 @@
+//https://www.cppstories.com/2021/evaluation-order-cpp17/
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string s = "but I have heard it works even"
+                    "if you don't believe in it";
+    s.replace(0, 4, "")
+     .replace(s.find("even"), 4, "only")
+     .replace(s.find(" don't"), 6, "");
+    std::cout << s;
+}
