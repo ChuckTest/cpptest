86e4e47f78e9b48cd1a798faaecd495df899549f
Chuck LU <chucklu2015@outlook.com>
2025-06-13 20:37:28 +0800

    format code

--- 0605/ch04/4-2.cpp
+++ 0605/ch04/4-2.cpp
@@ -1,31 +1,39 @@
-﻿#include "iostream"
+﻿#include <windows.h>
+#include "iostream"
 using namespace std;
 class Point
 {
-   public:
-       Point(int xx=0,int yy=0){X=xx; Y=yy;}
-       Point(Point&  p);
-       int GetX() {return X;}
-       int GetY() {return Y;}
-	   void SetX(int x) {X=x;}
-	   void SetY(int y) {Y=y;}
-   private:
-       int  X,Y;
+public:
+    Point(int xx = 0, int yy = 0)
+    {
+        X = xx;
+        Y = yy;
+    }
+    Point(Point &p);
+    int GetX() { return X; }
+    int GetY() { return Y; }
+    void SetX(int x) { X = x; }
+    void SetY(int y) { Y = y; }
+
+private:
+    int X, Y;
 };
 
-Point::Point (Point&  p)
+Point::Point(Point &p)
 {
-      X=p.X;
-      Y=p.Y;
-      cout<<"复制构造函数被调用"<<endl;
+    X = p.X;
+    Y = p.Y;
+    cout << "复制构造函数被调用" << endl;
 }
 
-void main(){
-	Point myP(12,12);
-	cout<<"第一个点（"<<myP.GetX()<<", "<<myP.GetY()<<"）"<<endl;
-	Point myP1(myP);
-	cout<<"第二个点（"<<myP1.GetX()<<", "<<myP1.GetY()<<"）"<<endl;
-	myP1.SetX(100);
-	myP1.SetY(100);
-	cout<<"重新赋值后，第二个点（"<<myP1.GetX()<<", "<<myP1.GetY()<<"）"<<endl;
+int main()
+{
+    SetConsoleOutputCP(65001); // 控制台输出 UTF-8
+    Point myP(12, 12);
+    cout << "第一个点（" << myP.GetX() << ", " << myP.GetY() << "）" << endl;
+    Point myP1(myP);
+    cout << "第二个点（" << myP1.GetX() << ", " << myP1.GetY() << "）" << endl;
+    myP1.SetX(100);
+    myP1.SetY(100);
+    cout << "重新赋值后，第二个点（" << myP1.GetX() << ", " << myP1.GetY() << "）" << endl;
 }
--- 0605/ch04/4-3.cpp
+++ 0605/ch04/4-3.cpp
@@ -1,56 +1,58 @@
-﻿#include <iostream>
+﻿#include <windows.h>
+#include <iostream>
 using namespace std;
 const float PI = 3.14159;
 const float FencePrice = 35;
 const float ConcretePrice = 20;
 
-//声明类Circle 及其数据和方法
+// 声明类Circle 及其数据和方法
 class Circle
 {
-  private:
-    float   radius;
-       
-  public:
-    Circle(float r);  //构造函数，初始化半径
-        
-    float Circumference(); //圆周长
-    float Area();  //圆面积
+private:
+  float radius;
+
+public:
+  Circle(float r); // 构造函数，初始化半径
+
+  float Circumference(); // 圆周长
+  float Area();          // 圆面积
 };
 // 类的实现
 // 构造函数初始化数据成员radius
 Circle::Circle(float r)
 {
-	radius=r;
+  radius = r;
 }
 
 // 计算圆的周长
-float Circle::Circumference() 
+float Circle::Circumference()
 {
-    return 2 * PI * radius;
+  return 2 * PI * radius;
 }
-        
-// 计算圆的面积 
-float Circle::Area() 
+
+// 计算圆的面积
+float Circle::Area()
 {
-    return PI * radius * radius;
+  return PI * radius * radius;
 }
-void main ()
+int main()
 {
+  SetConsoleOutputCP(65001); // 控制台输出 UTF-8
   float radius;
-  float FenceCost, ConcreteCost; 
-  
+  float FenceCost, ConcreteCost;
+
   // 提示用户输入半径
-  cout<<"Enter the radius of the pool: ";
-  cin>>radius;
+  cout << "Enter the radius of the pool: ";
+  cin >> radius;
 
   // 声明 Circle 对象
   Circle Pool(radius);
-  Circle PoolRim(radius + 3);       
-// 计算栅栏造价并输出
-    FenceCost = PoolRim.Circumference() * FencePrice;
-    cout << "Fencing Cost is ￥" << FenceCost << endl;
-    
-    //  计算过道造价并输出
-    ConcreteCost = (PoolRim.Area() - Pool.Area())*ConcretePrice;
-    cout << "Concrete Cost is ￥" << ConcreteCost << endl;
+  Circle PoolRim(radius + 3);
+  // 计算栅栏造价并输出
+  FenceCost = PoolRim.Circumference() * FencePrice;
+  cout << "Fencing Cost is ￥" << FenceCost << endl;
+
+  //  计算过道造价并输出
+  ConcreteCost = (PoolRim.Area() - Pool.Area()) * ConcretePrice;
+  cout << "Concrete Cost is ￥" << ConcreteCost << endl;
 }
