2b59601e3ac82876b17fa946a385081cb2226a10
Chuck LU <chucklu2015@outlook.com>
2025-06-14 20:05:46 +0800

    update code

--- 0605/ch03/3-18.cpp
+++ 0605/ch03/3-18.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-//用递归法计算从n个人中选择k个人的排列组合数
+// 用递归法计算从n个人中选择k个人的排列组合数
 /*
 我们任选一个人（设为 A），然后有两种情况：
 
@@ -18,13 +18,14 @@ int comm(int n, int k)
     if (k == 0 || k == n)
         return 1; // 组合数的边界条件
     if (k > n)
-        return 0;                               // 如果k大于n，返回0
-    return comm(n - 1, k - 1) + comm(n - 1, k); // 递归计算组合数
+        return 0;                                                       // 如果k大于n，返回0
+    cout << n - 1 << "," << k - 1 << "  " << n - 1 << "," << k << endl; // 输出递归调用的参数
+    return comm(n - 1, k - 1) + comm(n - 1, k);                         // 递归计算组合数
 }
 
 int main()
 {
-    int n = 18, k = 5;
+    int n = 2, k = 2;
     cout << "C(" << n << "," << k << ") = " << comm(n, k) << endl; // 输出组合数
     return 0;
 }
