4989e1111ab47f9779feed007c08e7577af45b7e
Chuck LU <chucklu2015@outlook.com>
2025-06-14 22:12:43 +0800

    add comments

--- test/2025/06/test_2025_0614_002.cpp
+++ test/2025/06/test_2025_0614_002.cpp
@@ -3,17 +3,17 @@ using namespace std;
 class A
 {
 public:
-    A(int i);
-    void print();
-    const int &r;
+    A(int i);     // 构造函数声明
+    void print(); // 成员函数声明
+    const int &r; // 常引用成员（公有）
 
 private:
-    const int a;
-    static const int b; // 静态常数据成员
+    const int a;        // 常数据成员（私有）
+    static const int b; // 静态常数据成员（私有）
 };
 
-const int A::b = 10;
-A::A(int i) : a(i), r(a) {}
+const int A::b = 10;        // 在类外定义并初始化静态常数据成员
+A::A(int i) : a(i), r(a) {} // 构造函数实现，使用初始化列表给常数据成员和常引用成员赋值
 void A::print()
 {
     cout << a << ": " << b << ": " << r << endl;
