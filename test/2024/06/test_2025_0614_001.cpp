c4f71bb3c2edf056b5f2182e52e6ecf0b6b1c80e
Chuck LU <chucklu2015@outlook.com>
2025-06-14 20:15:15 +0800

    cout address of each items in char array

--- test/2024/06/test_2025_0614_001.cpp
+++ test/2024/06/test_2025_0614_001.cpp
@@ -6,11 +6,19 @@ int main()
     char *p = "Hello";
     cout << *p << endl;
     cout << *(p + 1) << endl;
-    cout << p <<endl;
+    cout << p << endl;
+    cout << static_cast<void *>(p) << endl;
 
     char array[] = "Hello";
     cout << array << endl;
     cout << *(array + 1);
 
+    cout << "数组首地址（array）: " << static_cast<void *>(array) << endl;
+    // 打印每个字符的地址和内容
+    for (int i = 0; i < sizeof(array); ++i)
+    {
+        cout << "array[" << i << "] = '" << array[i] << "'"
+             << " 地址: " << static_cast<void *>(&array[i]) << endl;
+    }
     return 0;
 }
