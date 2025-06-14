ea276f685dc4427f87fba2f225ea7ceec9b7cf2c
Chuck LU <chucklu2015@outlook.com>
2025-06-14 22:00:46 +0800

    code fix

--- test/2025/06/test_2025_0614_002.cpp
+++ test/2025/06/test_2025_0614_002.cpp
@@ -10,18 +10,19 @@ public:
 private:
     const int a;
     static const int b; // 静态常数据成员
-}；
+};
 
-    const int A::b = 10;
+const int A::b = 10;
 A::A(int i) : a(i), r(a) {}
 void A::print()
 {
     cout << a << ": " << b << ": " << r << endl;
 }
-void main()
+int main()
 {
     /*建立对象a1和a2，并以100和0作为初值，分别调用构造函数，通过构造函数的初始化列表给对象的常数据成员赋初值*/
     A a1(100), a2(0);
     a1.print();
     a2.print();
+    return 0;
 }
