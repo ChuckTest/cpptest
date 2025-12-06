dacbc00725f695ab742d67e0d01dcc6aad706f43
Chuck LU <chucklu2015@outlook.com>
2025-12-07 02:15:58 +0800

    set console encoding to utf-8

--- 1204/main.cpp
+++ 1204/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+
+#include <windows.h>
+
 #include "linkedlist.h"
-using namespace std;
+
 int main()
 {
+    // 设置控制台输出 UTF-8 编码
+    SetConsoleOutputCP(65001);
+    cout << "hello world!" << endl;
+
     LinkList list;
-    //cout << list.GetSize() << endl;
+    cout << list.GetSize() << endl;
     list.CreateListR();
+    list.PrintList();
+    system("pause");
     return 0;
 }
