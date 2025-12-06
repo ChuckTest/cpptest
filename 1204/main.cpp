e4731574410f8afe35ae9667069a83a009affdee
Chuck LU <chucklu2015@outlook.com>
2025-12-07 02:26:57 +0800

    query the data by data value

--- 1204/main.cpp
+++ 1204/main.cpp
@@ -14,12 +14,30 @@ int main()
     cout << list.GetSize() << endl;
     list.CreateListR();
     list.PrintList();
+    /*
     cout << "你想查询第几个节点？" << endl;
     int i;
     cin >> i;
     cout << "******查询结果******" << endl;
     cout << list.GetElement(i);
     cout << endl;
+    */
+
+    cout << "你想查询data=?的数据？" << endl;
+    int data;
+    cin >> data;
+    cout << "******查询结果******" << endl;
+    Node *result = list.Search(data);
+    if (result != NULL)
+    {
+        cout << "查找成功" << endl;
+    }
+    else
+    {
+
+        cout << "找不到data=" << data << "的节点" << endl;
+    }
+
     system("pause");
     return 0;
 }
