17c9b60990d9e8661ea51183af4435de7b7cc83c
Chuck LU <chucklu2015@outlook.com>
2025-12-29 17:32:05 +0800

    add class Node and LinkList

--- 1229/21/main.cpp
+++ 1229/21/main.cpp
@@ -3,6 +3,41 @@
 #include <windows.h>
 using namespace std;
 
+class Node
+{
+    friend class LinkList;
+
+private:
+    int data;
+    Node *next;
+
+public:
+    Node(Node *ptr = NULL)
+    {
+        next = ptr;
+    }
+    Node(int item, Node *ptr = NULL)
+    {
+        data = item;
+        next = ptr;
+    }
+};
+
+class LinkList
+{
+private:
+    Node *head = NULL;
+    Node *tail = NULL;
+
+public:
+    LinkList()
+    {
+        head = new Node();
+        head->next = NULL;
+        tail = head;
+    }
+};
+
 int main()
 {
     // 设置控制台输出 UTF-8 编码
