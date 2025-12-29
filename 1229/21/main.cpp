35b850772b28b022d18f5b16352fb8d3f39f5a39
Chuck LU <chucklu2015@outlook.com>
2025-12-29 17:36:45 +0800

    create the list and print it

--- 1229/21/main.cpp
+++ 1229/21/main.cpp
@@ -36,6 +36,38 @@ public:
         head->next = NULL;
         tail = head;
     }
+
+    void CreateListR()
+    {
+        int data;
+        cout << "请输入数据(输入小于等于0结束):" << endl;
+        cin >> data;
+
+        Node *s;
+        while (data > 0)
+        {
+            s = new Node(data, NULL);
+            tail->next = s;
+            tail = s;
+            cout << "请再次输入数据(输入小于等于0结束):" << endl;
+            cin >> data;
+        }
+    }
+
+    void PrintList()
+    {
+        Node *current = head->next;
+        while (current != NULL)
+        {
+            cout << current->data;
+            current = current->next;
+            if (current != NULL)
+            {
+                cout << "->";
+            }
+        }
+        cout << endl;
+    }
 };
 
 int main()
@@ -43,6 +75,10 @@ int main()
     // 设置控制台输出 UTF-8 编码
     SetConsoleOutputCP(65001);
     cout << "单链表实验" << endl;
+
+    LinkList list;
+    list.CreateListR();
+    list.PrintList();
 
     system("pause");
     return 0;
