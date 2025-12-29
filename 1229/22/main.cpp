c1d7d6cce39cc1749846172d62b03c33e47b844a
Chuck LU <chucklu2015@outlook.com>
2025-12-29 17:36:28 +0800

    update title

--- 1229/22/main.cpp
+++ 1229/22/main.cpp
@@ -7,7 +7,7 @@ int main()
 {
     // 设置控制台输出 UTF-8 编码
     SetConsoleOutputCP(65001);
-    cout << "单链表实验" << endl;
+    cout << "实现二叉排序树" << endl;
 
     system("pause");
     return 0;
--- 1229/23/main.cpp
+++ 1229/23/main.cpp
@@ -7,7 +7,7 @@ int main()
 {
     // 设置控制台输出 UTF-8 编码
     SetConsoleOutputCP(65001);
-    cout << "单链表实验" << endl;
+    cout << "用循环对立解决舞伴问题" << endl;
 
     system("pause");
     return 0;
